dec5cafdbaeb362ad35f8b410de7f872e944c08a
yoshoku <yoshoku@outlook.com>
2025-10-01 12:56:34 +0900

    feat: add minimize module function

--- lib/numo/optimize.rb
+++ lib/numo/optimize.rb
@@ -5,9 +5,106 @@ require 'numo/narray'
 require_relative 'optimize/version'
 require_relative 'optimize/optimize'
 
+# Ruby/Numo (NUmerical MOdules)
 module Numo
+  # Numo::Optimize provides functions for minimizing objective functions.
   module Optimize
-    class Error < StandardError; end
-    # Your code goes here...
+    # Lbfgsb module provides functions for minimization using L-BFGS-B algorithm.
+    module Lbfgsb
+      module_function
+
+      # @!visibility private
+      def fnc(fnc, x, args)
+        if args.is_a?(Hash)
+          fnc.call(x, **args)
+        elsif args.is_a?(Array)
+          fnc.call(x, *args)
+        elsif args.nil?
+          fnc.call(x)
+        else
+          fnc.call(x, args)
+        end
+      end
+
+      # @!visibility private
+      def jcb(jcb, x, args)
+        if args.is_a?(Hash)
+          jcb.call(x, **args)
+        elsif args.is_a?(Array)
+          jcb.call(x, *args)
+        elsif args.nil?
+          jcb.call(x)
+        else
+          jcb.call(x, args)
+        end
+      end
+
+      private_class_method :fnc, :jcb
+    end
+
+    module_function
+
+    # Minimize the given function.
+    #
+    # @param fnc [Method/Proc] Method for calculating the function to be minimized.
+    # @param x_init [Numo::DFloat] (shape: [n_elements]) Initial point.
+    # @param jcb [Method/Proc/Boolean] Method for calculating the gradient vector.
+    #   If true is given, fnc is assumed to return the function value and gardient vector as [f, g] array.
+    # @param method [String] Type of algorithm. Now only 'L-BFGS-B' is supported.
+    # @param args [Object] Arguments pass to the 'fnc' and 'jcb'.
+    # @param bounds [Numo::DFloat/Nil] (shape: [n_elements, 2])
+    #   \[lower, upper\] bounds for each element x. If nil is given, x is unbounded.
+    # @param factr [Float] The iteration will be stop when
+    #
+    #   (f^k - f^\{k+1\})/max{|f^k|,|f^\{k+1\}|,1} <= factr * Lbfgsb::DBL_EPSILON
+    #
+    #   Typical values for factr: 1e12 for low accuracy; 1e7 for moderate accuracy; 1e1 for extremely high accuracy.
+    # @param pgtol [Float] The iteration will be stop when
+    #
+    #   max{|pg_i| i = 1, ..., n} <= pgtol
+    #
+    #   where pg_i is the ith component of the projected gradient.
+    # @param maxcor [Integer] The maximum number of variable metric corrections used to define the limited memory matrix.
+    # @param maxiter [Integer] The maximum number of iterations.
+    # @param verbose [Integer/Nil] If negative value or nil is given, no display output is generated.
+    # @return [Hash] Optimization results; { x:, n_fev:, n_jev:, n_iter:, fnc:, jcb:, task:, success: }
+    #   - x [Numo::DFloat] Updated vector by optimization.
+    #   - n_fev [Interger] Number of calls of the objective function.
+    #   - n_jev [Integer] Number of calls of the jacobian.
+    #   - n_iter [Integer] Number of iterations.
+    #   - fnc [Float] Value of the objective function.
+    #   - jcb [Numo::Narray] Values of the jacobian
+    #   - task [String] Description of the cause of the termination.
+    #   - success [Boolean] Whether or not the optimization exited successfully.
+    def minimize(fnc:, x_init:, jcb:, method: 'L-BFGS-B', args: nil, bounds: nil, factr: 1e7, pgtol: 1e-5, # rubocop:disable Lint/UnusedMethodArgument
+                 maxcor: 10, maxiter: 15_000, verbose: nil)
+      n_elements = x_init.size
+      l = Numo::DFloat.zeros(n_elements)
+      u = Numo::DFloat.zeros(n_elements)
+      nbd = if Numo::Optimize::Lbfgsb::SZ_F77_INTEGER == 64
+              Numo::Int64.zeros(n_elements)
+            else
+              Numo::Int32.zeros(n_elements)
+            end
+
+      unless bounds.nil?
+        n_elements.times do |n|
+          lower = bounds[n, 0]
+          upper = bounds[n, 1]
+          l[n] = lower
+          u[n] = upper
+          if lower.finite? && !upper.finite?
+            nbd[n] = 1
+          elsif lower.finite? && upper.finite?
+            nbd[n] = 2
+          elsif !lower.finite? && upper.finite?
+            nbd[n] = 3
+          end
+        end
+      end
+
+      Numo::Optimize::Lbfgsb.fmin(fnc, x_init.dup, jcb, args, l, u, nbd, maxcor,
+                                  factr, pgtol, maxiter, verbose)
+    end
   end
 end
--- test/numo/test_optimize.rb
+++ test/numo/test_optimize.rb
@@ -3,9 +3,172 @@
 require 'test_helper'
 
 module Numo
-  class TestOptimize < Minitest::Test
-    def test_that_it_has_a_version_number
-      refute_nil ::Numo::Optimize::VERSION
+  class TestOptimize < Minitest::Test # rubocop:disable Metrics/ClassLength
+    def test_minimize_lbfgbs_driver1
+      n = 25
+      m = 5
+      x = Numo::DFloat.zeros(n) + 3
+      b = Numo::DFloat.zeros(n, 2)
+      0.step(n - 1, 2) do |i|
+        b[i, 0] = 1
+        b[i, 1] = 100
+      end
+      1.step(n - 1, 2) do |i|
+        b[i, 0] = -100
+        b[i, 1] = 100
+      end
+      fnc = proc do |x, n|
+        f = 0.25 * ((x[0] - 1)**2)
+        (1...n).each do |i|
+          f += (x[i] - (x[i - 1]**2))**2
+        end
+        f * 4.0
+      end
+      jcb = proc do |x, n|
+        g = Numo::DFloat.zeros(n)
+        t1 = x[1] - (x[0]**2)
+        g[0] = (2.0 * (x[0] - 1.0)) - (16.0 * x[0] * t1)
+        (1...(n - 1)).each do |i|
+          t2 = t1
+          t1 = x[i + 1] - (x[i]**2)
+          g[i] = (8.0 * t2) - (16.0 * x[i] * t1)
+        end
+        g[n - 1] = t1 * 8.0
+        g
+      end
+      result = Numo::Optimize.minimize(fnc: fnc, x_init: x, jcb: jcb, args: n,
+                                       bounds: b, maxcor: m, verbose: -1)
+
+      assert(result[:success])
+      assert(result[:task].start_with?('CONV'))
+      assert_equal(23, result[:n_iter])
+      assert_in_delta(1.0834900834300615e-09, result[:fnc], 1e-10)
+      assert_kind_of(Numo::DFloat, result[:x])
+      assert_kind_of(Numo::DFloat, result[:jcb])
+
+      fnc = proc do |x, n|
+        # calculate function value
+        f = 0.25 * ((x[0] - 1)**2)
+        (1...n).each do |i|
+          f += (x[i] - (x[i - 1]**2))**2
+        end
+        f *= 4.0
+        # calculate gradient vector
+        g = Numo::DFloat.zeros(n)
+        t1 = x[1] - (x[0]**2)
+        g[0] = (2.0 * (x[0] - 1.0)) - (16.0 * x[0] * t1)
+        (1...(n - 1)).each do |i|
+          t2 = t1
+          t1 = x[i + 1] - (x[i]**2)
+          g[i] = (8.0 * t2) - (16.0 * x[i] * t1)
+        end
+        g[n - 1] = t1 * 8.0
+        # return set of value and vector
+        [f, g]
+      end
+      result = Numo::Optimize.minimize(fnc: fnc, x_init: x, jcb: true, args: n,
+                                       bounds: b, maxcor: m, verbose: -1)
+
+      assert(result[:success])
+      assert(result[:task].start_with?('CONV'))
+      assert_equal(23, result[:n_iter])
+      assert_in_delta(1.0834900834300615e-09, result[:fnc], 1e-10)
+      assert_kind_of(Numo::DFloat, result[:x])
+      assert_kind_of(Numo::DFloat, result[:jcb])
+    end
+
+    def test_minimize_lbfgbs_driver2
+      n = 25
+      m = 5
+      pgtol = (5.80702e-15 + 1) * 1e-10
+      x = Numo::DFloat.zeros(n) + 3
+      b = Numo::DFloat.zeros(n, 2)
+      0.step(n - 1, 2) do |i|
+        b[i, 0] = 1
+        b[i, 1] = 100
+      end
+      1.step(n - 1, 2) do |i|
+        b[i, 0] = -100
+        b[i, 1] = 100
+      end
+      fnc = proc do |x, n|
+        f = 0.25 * ((x[0] - 1)**2)
+        (1...n).each do |i|
+          f += (x[i] - (x[i - 1]**2))**2
+        end
+        f * 4.0
+      end
+      jcb = proc do |x, n|
+        g = Numo::DFloat.zeros(n)
+        t1 = x[1] - (x[0]**2)
+        g[0] = (2.0 * (x[0] - 1.0)) - (16.0 * x[0] * t1)
+        (1...(n - 1)).each do |i|
+          t2 = t1
+          t1 = x[i + 1] - (x[i]**2)
+          g[i] = (8.0 * t2) - (16.0 * x[i] * t1)
+        end
+        g[n - 1] = t1 * 8.0
+        g
+      end
+      result = Numo::Optimize.minimize(fnc: fnc, x_init: x, jcb: jcb, args: n,
+                                       bounds: b, maxcor: m, factr: 0, pgtol: pgtol,
+                                       verbose: -1)
+
+      assert(result[:success])
+      assert(result[:task].start_with?('CONVERGENCE: NORM_OF_PROJECTED_GRADIENT_<=_PGTOL'))
+      assert_equal(46, result[:n_iter])
+      assert_in_delta(5.80702e-15, result[:fnc], 1e-15)
+      assert_kind_of(Numo::DFloat, result[:x])
+      assert_kind_of(Numo::DFloat, result[:jcb])
+    end
+
+    def test_minimize_lbfgbs_driver3
+      n = 1000
+      m = 10
+      pgtol = (5.35267e-22 + 1) * 1e-10
+      x = Numo::DFloat.zeros(n) + 3
+      b = Numo::DFloat.zeros(n, 2)
+      0.step(n - 1, 2) do |i|
+        b[i, 0] = 1
+        b[i, 1] = 100
+      end
+      1.step(n - 1, 2) do |i|
+        b[i, 0] = -100
+        b[i, 1] = 100
+      end
+      fnc = proc do |x|
+        n = x.size
+        f = 0.25 * ((x[0] - 1)**2)
+        (1...n).each do |i|
+          f += (x[i] - (x[i - 1]**2))**2
+        end
+        f * 4.0
+      end
+      jcb = proc do |x|
+        n = x.size
+        g = Numo::DFloat.zeros(n)
+        t1 = x[1] - (x[0]**2)
+        g[0] = (2.0 * (x[0] - 1.0)) - (16.0 * x[0] * t1)
+        (1...(n - 1)).each do |i|
+          t2 = t1
+          t1 = x[i + 1] - (x[i]**2)
+          g[i] = (8.0 * t2) - (16.0 * x[i] * t1)
+        end
+        g[n - 1] = t1 * 8.0
+        g
+      end
+      result = Numo::Optimize.minimize(fnc: fnc, x_init: x, jcb: jcb, args: n,
+                                       bounds: b, maxcor: m, factr: 0, pgtol: pgtol,
+                                       verbose: -1)
+
+      assert(result[:success])
+      assert(result[:task].start_with?('CONVERGENCE: NORM_OF_PROJECTED_GRADIENT_<=_PGTOL'))
+      assert_equal(49, result[:n_iter])
+      assert_in_delta(5.35267e-22, result[:fnc], 1e-22)
+      assert_kind_of(Numo::DFloat, result[:x])
+      assert_kind_of(Numo::DFloat, result[:jcb])
+      assert_equal(n, result[:x].size)
+      assert_equal(n, result[:jcb].size)
     end
   end
 end
