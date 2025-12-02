9255a7651731b4a3d7a0b72591e8daad62ce5e0c
yoshoku <yoshoku@outlook.com>
2025-12-02 21:29:07 +0900

    chore!: remove visibility option

--- ext/numo/optimize/extconf.rb
+++ ext/numo/optimize/extconf.rb
@@ -37,9 +37,4 @@ end
 
 $VPATH << '$(srcdir)/src'
 
-# Makes all symbols private by default to avoid unintended conflict
-# with other gems. To explicitly export symbols you can use RUBY_FUNC_EXPORTED
-# selectively, or entirely remove this flag.
-append_cflags('-fvisibility=hidden')
-
 create_makefile('numo/optimize/optimize')
